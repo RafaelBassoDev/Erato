c4ed189a251c7b0f3182030662d30f56c635f492
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-02-01 17:18:41 -0300

    ReadingScreen (feat): create base swiftui view

---
+++ Erato/Screens/ReadingScreen/ReadingScreen.swift
@@ -0,0 +1,18 @@
+//
+//  ReadingScreen.swift
+//  Erato
+//
+//  Created by Rafael Basso on 01/02/24.
+//
+
+import SwiftUI
+
+struct ReadingScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ReadingScreen()
+}
