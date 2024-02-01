1d65ec3acb0e9e778c213de5e32f2a936e40d224
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-02-01 19:00:11 -0300

    NovelCollection (feat): add base swiftui view and model

---
+++ Erato/Screens/Model/Novel.swift
@@ -0,0 +1,14 @@
+//
+//  Novel.swift
+//  Erato
+//
+//  Created by Rafael Basso on 01/02/24.
+//
+
+import Foundation
+
+struct Novel {
+    let title: String
+    let description: String
+    let chapters: [Chapter]
+}
---
+++ Erato/Screens/NovelCollection/NovelCollection.swift
@@ -0,0 +1,18 @@
+//
+//  NovelCollection.swift
+//  Erato
+//
+//  Created by Rafael Basso on 01/02/24.
+//
+
+import SwiftUI
+
+struct NovelCollection: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NovelCollection()
+}
