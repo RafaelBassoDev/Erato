4335c6f88f61a9d93c6032dccd8a3e1d90772b0b
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-02-02 13:42:23 -0300

    NovelDetails (feat): add base swiftui view and model

---
+++ Erato/Screens/NovelDetails/NovelDetails.swift
@@ -0,0 +1,18 @@
+//
+//  NovelDetails.swift
+//  Erato
+//
+//  Created by Rafael Basso on 02/02/24.
+//
+
+import SwiftUI
+
+struct NovelDetails: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NovelDetails()
+}
