d7dfed9b81d222c2f791f1f425acf0eec5ca1926
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-02-07 10:03:07 -0300

    Extensions (feat): add linear style loading to View extension

--- Erato/Extensions/View.swift
+++ Erato/Extensions/View.swift
@@ -9,12 +9,29 @@ import SwiftUI
 
 extension View {
     @ViewBuilder
-    func loading(_ isLoading: Bool, scale: CGFloat = 2.0) -> some View {
+    func loading(_ isLoading: Bool, label: String = "", scale: CGFloat = 2.0) -> some View {
         if isLoading {
             ZStack {
-                ProgressView()
+                ProgressView(label)
                     .scaleEffect(scale, anchor: .center)
                     .progressViewStyle(.circular)
+                
+                self
+                    .disabled(isLoading)
+                    .foregroundStyle(.foreground.opacity(0.1))
+            }
+        } else {
+            self
+        }
+    }
+
+    @ViewBuilder
+    func loading(_ isLoading: Bool, progress: CGFloat , label: String = "", scale: CGFloat = 0.9) -> some View {
+        if isLoading {
+            ZStack {
+                ProgressView(label, value: progress)
+                    .scaleEffect(scale, anchor: .center)
+                    .progressViewStyle(.linear)
                 
                 self
                     .disabled(isLoading)
