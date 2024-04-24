1e60637dc796bd5755d173a470312923e65a16e8
Rafael Basso <rafaelbasso2008@hotmail.com>
2024-04-24 17:21:03 +0200

    ReadingScreen (feat): create 'ReadingScreenDelegate'.

---
+++ Erato/Screens/ReadingScreen/ReadingScreenDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  ReadingScreenDelegate.swift
+//  Erato
+//
+//  Created by Rafael Basso on 24/04/24.
+//
+
+import Foundation
+
+protocol ReadingScreenDelegate {
+    func didClickPrevious()
+    func didClickNext()
+    func didClickChapterList()
+}
